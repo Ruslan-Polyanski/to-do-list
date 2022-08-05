e6ea492fae5036ae2d8081e91aeafb93ce096bd6
Ruslan Polyanski <ruslapolyanski@yandex.by>
2022-08-05 20:25:22 +0300

    feat: print todos

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 
 let users = [];
 let todos = [];
+let todoList = document.getElementById('todo-list');
 
 document.addEventListener('DOMContentLoaded', initApp);
+
+function getUserName(userId){
+    let user = users.find(user => user.id === userId);
+    return user.name;
+}
+
+function printTodo({id, userId, title, completed}){
+    let li = document.createElement('li');
+    li.className = 'todo-item';
+    li.dataset.id = id;
+    li.innerHTML = `<span>${title} by <b>${getUserName(userId)}</b></span>`;
+
+    let status = document.createElement('input');
+    status.type = 'checkbox';
+    status.checked = completed;
+
+    let close = document.createElement('span');
+    close.innerHTML = '&times;';
+    close.className = 'close';
+
+    li.prepend(status);
+    li.append(close);
+
+    todoList.prepend(li);
+}
 
 function initApp(){
     Promise.all([getAllUsers(), getAllTodos()]).then(values => {
         [users, todos] = values;
+        todos.forEach(todo => printTodo(todo))
     })
 }
 
@@ -21,3 +48,6 @@ async function getAllTodos(){
     let date = await response.json();
     return date;
 }
+
+
+
